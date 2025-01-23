0e7e9c54d65b8a5e2a005366b6ceae822fc062ab
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-01-23 17:32:36 +0100

    Create Featured Books Cubit States

---
+++ bookly_app/lib/features/home/presentation/view_models/featured_books_cubit/featured_books_state.dart
@@ -0,0 +1,20 @@
+part of 'featured_books_cubit.dart';
+
+@immutable
+sealed class FeaturedBooksState {}
+
+final class FeaturedBooksInitial extends FeaturedBooksState {}
+
+final class FeaturedBooksLoading extends FeaturedBooksState {}
+
+final class FeaturedBooksFailure extends FeaturedBooksState {
+  final String errorMsg;
+
+  FeaturedBooksFailure(this.errorMsg);
+}
+
+final class FeaturedBooksSucces extends FeaturedBooksState {
+  final List<BookModel> books;
+
+  FeaturedBooksSucces(this.books);
+}
